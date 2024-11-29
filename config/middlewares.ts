358ccadd452a939318d567cfdd3fd37389657f92
cmrmont <cmonterora@gmail.com>
2024-11-28 20:08:08 -0500

    proyecto ecommerce de cafe avance v3

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,18 @@
-export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+module.exports = [
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["http://localhost:3000"], // Agrega la URL de tu frontend aquí
+      methods: ["GET", "POST", "PUT", "DELETE"],
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
